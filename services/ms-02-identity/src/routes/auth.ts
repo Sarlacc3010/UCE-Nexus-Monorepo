3eb75597961a10bf9c9b91cc22096e853e2fae55
Abel Navarrete <138503991+Sarlacc3010@users.noreply.github.com>
2026-07-02 23:08:36 -0500

    Revert "Automatic PR: QA -> main (Prod)"

--- services/ms-02-identity/src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getUsers, registerUser } from '../controllers/authController';
-
-const router = Router();
-
-router.get('/users', getUsers);
-router.post('/register', registerUser);
-
-export default router;
